2039cd4ba5a313826b0fe7cec85501d78cf5a91c
Niikou <reidneko@gmail.com>
2018-10-21 10:14:31 -0500

    Single boundary turns red.

---
+++ info2180-lab4-master/maze.js
@@ -0,0 +1,15 @@
+
+
+window.onload = function() {
+	
+    boundary = document.getElementById("boundary1");
+
+    boundary.onmouseover = function(){
+        boundary.classList.add("youlose");
+    };
+
+
+
+
+
+}
