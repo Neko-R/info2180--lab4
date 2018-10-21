90973a0f7db6d9b577a278ccc96f16e01faa2819
Niikou <reidneko@gmail.com>
2018-10-21 12:24:50 -0500

    Disallow cheating.

--- info2180-lab4-master/maze.js
+++ info2180-lab4-master/maze.js
@@ -1,13 +1,21 @@
 var boundaryTouched = false;
+var win = false;
+var boundaries;
+var endIcon;
+var startIcon;
+var statusText;
+var maze;
 
 window.onload = function() {
-	
+
     boundaries = document.querySelectorAll(".boundary");
     endIcon = document.getElementById("end");
     startIcon = document.getElementById("start");
-    statusText = document.getElementById("status")
+    statusText = document.getElementById("status");
+    maze = document.getElementById("maze");
 
     endIcon.onmouseover = checkWin;
+    start.onmouseover = noCheat;
     start.onclick = restartMaze;
 
     for (var i = 0; i < boundaries.length - 1; i++) { 
@@ -17,20 +25,24 @@ window.onload = function() {
 
 
 function touchedBoundary(){
-    boundaryTouched = true;
-    for (var i = 0; i < boundaries.length - 1; i++) { 
-		boundaries[i].classList.add("youlose");
-    };
-    statusText.textContent = "You Lose!";
+    if(!win){
+        boundaryTouched = true;
+        for (var i = 0; i < boundaries.length - 1; i++) { 
+		    boundaries[i].classList.add("youlose");
+        };
+        statusText.textContent = "You Lose!";
+    }
 }
 
 function checkWin(){
     if(!boundaryTouched){
         statusText.textContent = "You Win!";
+        win = true;
     };
 }
 
 function restartMaze(){
+    win = false;
     if(boundaryTouched){
         for (var i = 0; i < boundaries.length - 1; i++) { 
             boundaries[i].classList.remove("youlose");
@@ -38,4 +50,8 @@ function restartMaze(){
         boundaryTouched = false;
     }
     statusText.textContent = "Move your mouse over the 'S' to begin.";
+}
+
+function noCheat(){
+    maze.onmouseleave = touchedBoundary;  
 }
