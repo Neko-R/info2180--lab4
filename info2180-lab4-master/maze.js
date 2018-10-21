d48d0973453bda0d05b6ac2d0f95dd25ffcec44e
Niikou <reidneko@gmail.com>
2018-10-21 11:25:21 -0500

    Alerts on successful completion of maze.

--- info2180-lab4-master/maze.js
+++ info2180-lab4-master/maze.js
@@ -1,18 +1,27 @@
-
+var boundaryTouched = false;
 
 window.onload = function() {
 	
     boundaries = document.querySelectorAll(".boundary");
+    end = document.getElementById("end");
+
+    end.onmouseover = checkWin;
 
     for (var i = 0; i < boundaries.length - 1; i++) { 
 		boundaries[i].onmouseover = touchedBoundary;
-	}
+	};
 }
 
 
 function touchedBoundary(){
+    boundaryTouched = true;
     for (var i = 0; i < boundaries.length - 1; i++) { 
 		boundaries[i].classList.add("youlose");
-	}
+    };
+}
 
+function checkWin(){
+    if(!boundaryTouched){
+        alert("You win!");
+    };
 }
